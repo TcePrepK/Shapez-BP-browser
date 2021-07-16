40418b6d4ed48f0c10a82f0d249ab0a00b281c7d
TcePrepK <56453014+TcePrepK@users.noreply.github.com>
2021-07-16 15:28:49 +0300

    SASS Compiler setup done

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var sass = require('node-sass');
+// var sass = require('node-sass-middleware');
 
 var index = require('./routes/index');
 
@@ -15,16 +15,17 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 // uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+// app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(sass.middleware({
-  src: __dirname + '/sass/',
-  dest: __dirname + '/public/stylesheets'
-}));
+// app.use(sass({
+//   src: __dirname + '/public/scss/',
+//   dest: __dirname + '/public/stylesheets',
+//   log: function (severity, key, value) { winston.log(severity, 'node-saas-middleware   %s : %s', key, value); }
+// }));
 
 app.use('/', index);
 
