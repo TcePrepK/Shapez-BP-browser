6479010de62cc60caefb39effd072994a6764014
TcePrepK <56453014+TcePrepK@users.noreply.github.com>
2021-07-18 00:06:19 +0300

    More work on export done

--- routes/index.js
+++ routes/index.js
@@ -8,11 +8,11 @@ const exampleBps = require('../config/exampleBlueprints.json');
 const{ renderBlueprint } = require('../public/javascripts/render');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { blueprints: exampleBps.blueprints });
 });
-router.get('/:id', function(req, res, next) {
-  let bp = exampleBps.blueprints.find( b => b.url === req.params.id );
+router.get('/:id', function (req, res, next) {
+  let bp = exampleBps.blueprints.find(b => b.url === req.params.id);
   if(bp) {
     const user = {
       name: 'SkimnerPhi',
@@ -21,7 +21,6 @@ router.get('/:id', function(req, res, next) {
 
     const blueprint = Blueprint.importShrimpBP(bp.data);
     const description = 'Takes in one full belt of each color paint and outputs white paint back the same direction.\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Aliquam consequat tempor ipsum, a blandit libero. Ut eget vehicula sem, et maximus lectus. Proin a varius felis, eget imperdiet nisi. Morbi nec mi mi. Donec faucibus congue lectus, ac vulputate nulla tincidunt vitae. Aliquam scelerisque dui in tempor auctor. Ut finibus porta risus, non congue nulla malesuada sed. Nulla facilisi. Phasellus et sem ac sem ullamcorper imperdiet. Vestibulum varius lorem et lacus venenatis congue. Quisque venenatis egestas diam, quis suscipit mauris venenatis eget. Maecenas quis tempus mauris.'.split('\n');
-
 
     res.render('designView', { title: bp.name, author: bp.author, user, bpString: bp.data, stats: blueprint.stats, description });
   } else {
